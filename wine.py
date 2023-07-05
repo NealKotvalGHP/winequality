086e8a136993c0bdd96f630580bcc301bd3f73e1
Neal Kotval <nk04655@ad.georgiasouthern.edu>
2023-07-05 10:25:32 -0400

    removed comments

--- wine.py
+++ wine.py
@@ -6,54 +6,25 @@ from sklearn.metrics import accuracy_score
 
 filename = "wine_combined_cleaned.csv"  # Replace with the actual file name
 
-
-# Read the CSV file into a DataFrame
+#Decision Tree
 df = pd.read_csv(filename)
-
-# Print the DataFrame
-
-# print(df.head())
-
 X = df.iloc[:,:12].values
 y = df.iloc[:,-1].values
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-# Create a Decision Tree Classifier
 classifier = DecisionTreeClassifier()
-
-# Train the classifier
 classifier.fit(X_train, y_train)
-
-# Make predictions on the test set
 predictions = classifier.predict(X_test)
-
-# Evaluate the accuracy of the classifier
 accuracy = accuracy_score(y_test, predictions)
 print("DT Accuracy:", accuracy)
 
-# Read the CSV file into a DataFrame
+#Random Forest
 df = pd.read_csv(filename)
-
-# Print the DataFrame
-
-# print(df.head())
-
 X = df.iloc[:,:12].values
 y = df.iloc[:,-1].values
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-# Create a Decision Tree Classifier
 classifier = RandomForestClassifier()
-
-# Train the classifier
 classifier.fit(X_train, y_train)
-
-# Make predictions on the test set
 predictions = classifier.predict(X_test)
-
-# Evaluate the accuracy of the classifier
 accuracy = accuracy_score(y_test, predictions)
 
 print("RF Accuracy:", accuracy)
